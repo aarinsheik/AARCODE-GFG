6e1e854cf96c3cb31237acae6b9107f6d166beb3
aarinsheik <121597865+aarinsheik@users.noreply.github.com>
2023-08-31 18:40:57 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Middle of Three/middle-of-three.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int middle(int A, int B, int C){
+        //code here//Position this line where user code will be pasted.
+        if( (A>B and A<C) or (A<B and A>C) ) return A;
+        if((B>A and B<C) or (B>C and B<A)) return B;
+        if((C>B and C<A) or (C<B and C>A)) return C;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int A,B,C;
+        cin>>A>>B>>C;
+        Solution ob;
+        cout<<ob.middle(A,B,C) <<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
