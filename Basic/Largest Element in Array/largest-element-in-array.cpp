da20d0115159ec1c89c28798c1a025fb575604ef
Aarin Sheik M <121597865+aarinsheik@users.noreply.github.com>
2023-11-13 04:57:51 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Largest Element in Array/largest-element-in-array.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+public:
+    int largest(vector<int> &arr, int n)
+    {   
+        int a=-1e9;
+        for(int i=0 ; i<n ; i++){
+            a = max( a , arr[i] ) ;
+        }
+        return a;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int>arr(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        Solution ob;
+        cout << ob.largest(arr, n) << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
