935e01ef20022a86173dbcc9a22df69f86b768b3
Aarin Sheik M <121597865+aarinsheik@users.noreply.github.com>
2024-05-07 11:10:02 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Level order traversal Line by Line/level-order-traversal-line-by-line.cpp
@@ -0,0 +1,194 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+vector<vector<int>> levelOrder(struct Node* node);
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // for(string i:ip)
+    //     cout<<i<<" ";
+    // cout<<endl;
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+int main() {
+    int t;
+    scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+
+        vector<vector<int>> vec = levelOrder(root);
+        for(vector<int> v : vec){
+            for(int value : v){
+                cout<<value<<" ";
+            }
+            cout<<"$ ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* A binary tree Node
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+//Function to return the level order traversal line by line of a tree.
+vector<vector<int>> levelOrder(Node* node)
+{
+  //Your code here
+    queue<Node*> q1 , q2 ;
+    vector<vector<int>> res;
+    
+    q1.push(node);
+    int i=0;
+    
+    while( !q1.empty() or !q2.empty() ){
+        
+        if(i%2==0 and !q1.empty()){
+            
+            vector<int> k;
+            
+            while( !q1.empty() ){
+                
+                Node *t = q1.front();
+                q1.pop();
+                
+                k.push_back(t->data);
+                if(t->left)
+                    q2.push(t->left);
+                if(t->right)
+                    q2.push(t->right);
+                
+            }
+            
+            res.push_back(k);
+            
+        }
+        
+        else if(i%2==1 and !q2.empty()){
+            
+            vector<int> k;
+            
+            while( !q2.empty() ){
+                
+                Node *t = q2.front();
+                q2.pop();
+                
+                k.push_back(t->data);
+                
+                if(t->left)
+                    q1.push(t->left);
+                if(t->right)
+                    q1.push(t->right);
+                
+            }
+            
+            res.push_back(k);
+        }
+        
+        i++;
+        
+    }
+    
+    return res; 
+
+
+}
