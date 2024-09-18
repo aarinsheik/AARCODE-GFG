808baf656bbac35ef8be44e349e57c09970bbdbf
Aarin Sheik M <121597865+aarinsheik@users.noreply.github.com>
2024-09-18 20:17:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Swap two nibbles in a byte/swap-two-nibbles-in-a-byte.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int swapNibbles(int n) {
+        // code here
+        int right = n&15 ;
+        int left = n&240 ;
+        left = left>>4 ;
+        right = right<<4;
+        return (left|right);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.swapNibbles(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
