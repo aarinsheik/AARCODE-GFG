5caa1e7601044fb51b43099da210c9ea8d4722a2
Aarin Sheik M <121597865+aarinsheik@users.noreply.github.com>
2024-09-18 10:30:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Get min at pop/get-min-at-pop.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+stack<int> _push(int arr[],int n);
+
+void _getMinAtPop(stack<int>s);
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+//Function to push all the elements into the stack.
+stack<int> _push(int arr[],int n)
+{
+    // your code here
+    stack<int> st;
+    int mn=1e9;
+    for(auto i=0 ; i<n ; i++){
+        mn = min(mn,arr[i]);
+        st.push(mn);
+    }
+        
+    return st;
+}
+
+//Function to print minimum value in stack each time while popping.
+void _getMinAtPop(stack<int>st)
+{
+    // your code here
+    while(!st.empty()){
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+    
+}
+
+//{ Driver Code Starts.
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    stack<int>mys=_push(arr,n);
+	    _getMinAtPop(mys);
+	    
+	    cout<<endl;
+	    
+	}
+	return 0;
+}
+
+
+// } Driver Code Ends
