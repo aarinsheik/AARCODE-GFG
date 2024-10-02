63347d63964935df499cd47597963adf94c2f0aa
Aarin Sheik M <121597865+aarinsheik@users.noreply.github.com>
2024-10-02 19:20:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.py
@@ -0,0 +1,37 @@
+#User function Template for python3
+
+
+class Solution:
+    def find(self, arr, n, x):
+     
+        s=-1 
+        e=-1
+        fg=0 
+        
+        for i in range(n):
+            if(arr[i]==x and fg==0):
+                s=i
+                e=i
+                fg=1
+            if(fg==1 and i==n-1):
+                break
+            if(fg==1 and i<n and arr[i]==arr[i+1]):
+                e+=1
+            elif(fg==1 and i<n and arr[i]!=arr[i+1]):
+                break
+        
+        return (s,e)
+
+
+#{ 
+ # Driver Code Starts
+t=int(input())
+for _ in range(0,t):
+    l=list(map(int,input().split()))
+    n=l[0]
+    x=l[1]
+    arr=list(map(int,input().split()))
+    ob = Solution()
+    ans=ob.find(arr,n,x)
+    print(*ans)
+# } Driver Code Ends
