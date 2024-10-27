9b353f4cf1889759a680029c79b6b5cd68a97bcf
Aarin Sheik M <121597865+aarinsheik@users.noreply.github.com>
2024-10-28 02:17:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove duplicates in array/remove-duplicates-in-array.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> removeDuplicate(vector<int>& arr) {
+        // code here
+        
+        for(int i=arr.size()-1 ; i>=0 ; i--){
+            
+            int t=arr[i];
+            arr[i]=0;
+            auto it = find(arr.rbegin() , arr.rend() , t);
+            
+            if(it!=arr.rend()){
+                arr.erase(arr.begin()+i);
+            }
+            else{
+                arr[i]=t;
+            }
+            
+        }
+        
+        return arr;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.removeDuplicate(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
